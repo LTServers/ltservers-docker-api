6394125b3e1f14fd59f7faa87cccd6b448df6858
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-03-20 15:12:00 +0100

    return 400 instead of 404

--- src/routes/rcon.ts
+++ src/routes/rcon.ts
@@ -6,11 +6,11 @@ const router = Router();
 
 router.post("/:serverport", authMiddleware, async (req, res) => {
 	const { command } = req.body;
-	if (!command) return res.status(404).send({ message: "No command provided" });
+	if (!command) return res.status(400).send({ message: "No command provided" });
 
 	const { serverport } = req.params;
 	if (!serverport)
-		return res.status(404).send({ message: "No server port provided" });
+		return res.status(400).send({ message: "No server port provided" });
 
 	const rcon = await LTRcon.getRcon(parseInt(serverport, 10));
 	const rconRes = await rcon.send(command);
