7208e5fb6bafa0eb17d807689c55770ae4438fb4
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-07-28 20:36:18 +0200

    rcon route messages

--- src/routes/rcon.ts
+++ src/routes/rcon.ts
@@ -7,14 +7,21 @@ const router = Router();
 router.post("/:serverport", authMiddleware, async (req, res) => {
 	const { serverport } = req.params;
 	if (!serverport)
-		return res.status(400).send({ message: "No server port provided" });
+		return res
+			.status(400)
+			.json({ executed: false, message: "No server port provided" });
 
 	const { command } = req.body;
-	if (!command) return res.status(400).send({ message: "No command provided" });
+	if (!command)
+		return res
+			.status(400)
+			.json({ executed: false, message: "No command provided" });
 
 	const rcon = await LTRcon.getRcon(parseInt(serverport, 10));
 	if (!rcon)
-		return res.status(404).send({ message: "Rcon server not running" });
+		return res
+			.status(404)
+			.json({ executed: false, message: "Rcon server not running" });
 
 	const rconRes = await rcon.send(command);
 	res.json({ executed: true, message: rconRes });
